1b18c57c6fbbee62bd14190412b2615a72019027
Paulo <cicung2013@gmail.com>
2015-11-07 15:41:35 -0200

    Concluido o campo de enviar email.

--- contate-nos.php
+++ contate-nos.php
@@ -1,3 +1,27 @@
+<?php
+
+$para = "cicungnews@gmail.com";
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$estado = $_POST['estado'];
+$cidade = $_POST['cidade'];
+$mensagem = $_POST['mensagem'];
+$assunto = "CCNews";
+
+ $corpo = "Nome: $nome <br> E-mail: $email <br>";
+ $corpo .= "Telefone: $telefone <br> Estado: $estado <br>";
+ $corpo .=  "Cidade: $cidade <br> Mensagem: $mensagem <br>";
+
+$headers ="Content-Type: text/html; charset=UTF-8\n";
+
+  
+     mail($para, $assunto,$corpo, $headers);
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
